d856aa2b69622e8a1c4ae3dcba773a597557de3a
Jyri P <jyri.poussu@gmail.com>
2022-02-20 16:40:34 +0200

    Week 4, Exercise 5.2

--- viikko4/TddOstoskori/src/main/java/ohtu/Ostoskori.java
+++ viikko4/TddOstoskori/src/main/java/ohtu/Ostoskori.java
@@ -1,16 +1,26 @@
 package ohtu;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Ostoskori {
- 
+
+    private List<Ostos> shoppingList;
+
+    public Ostoskori() {
+        this.shoppingList = new ArrayList<>();
+    }
     public int tavaroitaKorissa() {
         // kertoo korissa olevien tavaroiden lukumäärän
         // eli jos koriin lisätty 2 kpl tuotetta "maito", 
         //   tulee metodin palauttaa 2 
         // jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", 
         //   tulee metodin palauttaa 2 
-        int itemAmount = 0;  
+        int itemAmount = 0;
+        
+        for(Ostos o: shoppingList) {
+            itemAmount+= o.lukumaara();
+        }
 
         return itemAmount;
     }
@@ -23,7 +33,7 @@ public class Ostoskori {
     }
  
     public void lisaaTuote(Tuote lisattava) {
-        // lisää tuotteen
+        shoppingList.add(new Ostos(lisattava));
     }
  
     public void poista(Tuote poistettava) {
